840f1ef4a9a735eacf8be8c2a2b3ffd01690e260
marcus6183 <marcusw6183@gmail.com>
2024-03-31 00:20:11 +0800

    Update home.blade.php
    - Make datatable responsive
    - Limit word shown in table view for Details (100 characters)

--- resources/views/product/home.blade.php
+++ resources/views/product/home.blade.php
@@ -12,40 +12,42 @@
                 <button class="h-10 px-4 bg-gray-100 rounded-md">Search</button>
             </form>
         </div>
-        <table class="max-w-full bg-white border">
-            <thead>
-                <tr>
-                    <th class="w-1/12 px-6 py-3 border text-left font-bold">No.</th>
-                    <th class="w-2/12 px-6 py-3 border text-left font-bold">Name</th>
-                    <th class="w-2/12 px-6 py-3 border text-left font-bold">Price (RM)</th>
-                    <th class="w-3/12 px-6 py-3 border text-left font-bold">Details</th>
-                    <th class="w-1/12 px-6 py-3 border text-left font-bold">Publish</th>
-                    <th class="w-3/12 px-6 py-3 border text-left font-bold">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($products as $product)
+        <div class="overflow-auto">
+            <table class="w-full bg-white border">
+                <thead>
                     <tr>
-                        <td class="px-6 py-4 border whitespace-nowrap">{{ $loop->iteration }}</td>
-                        <td class="px-6 py-4 border whitespace-nowrap">{{ $product->name }}</td>
-                        <td class="px-6 py-4 border whitespace-nowrap">{{ number_format($product->price, 2) }}</td>
-                        <td class="px-6 py-4 border whitespace-nowrap">{{ $product->details }}</td>
-                        <td class="px-6 py-4 border whitespace-nowrap">{{ $product->publish ? 'Yes' : 'No' }}</td>
-                        <td class="px-6 py-4 border whitespace-nowrap">
-                            <a class="inline-block h-12 px-4 py-3 bg-blue-300 text-white rounded-md"
-                                href="{{ route('product.show', $product) }}">Show</a>
-                            <a class="inline-block h-12 px-4 py-3 bg-blue-500 text-white rounded-md"
-                                href="{{ route('product.edit', $product) }}">Edit</a>
-                            <form class="inline m-0 p-0" action="{{ route('product.delete', $product) }}"
-                                method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button class="h-12 px-4 py-3 bg-red-500 text-white rounded-md">Delete</button>
-                            </form>
-                        </td>
+                        <th class="w-10 px-6 py-3 border text-left font-bold">No.</th>
+                        <th class="px-6 py-3 border text-left font-bold">Name</th>
+                        <th class="w-10 px-6 py-3 border text-left font-bold">Price (RM)</th>
+                        <th class="px-6 py-3 border text-left font-bold">Details</th>
+                        <th class="w-5 px-6 py-3 border text-left font-bold">Publish</th>
+                        <th class="w-20 px-6 py-3 border text-left font-bold">Action</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr>
+                            <td class="px-6 py-4 border whitespace-nowrap">{{ $loop->iteration }}</td>
+                            <td class="px-6 py-4 border whitespace-nowrap">{{ $product->name }}</td>
+                            <td class="px-6 py-4 border whitespace-nowrap">{{ number_format($product->price, 2) }}</td>
+                            <td class="px-6 py-4 border whitespace-nowrap">{{ Str::limit($product->details, 100) }}</td>
+                            <td class="px-6 py-4 border whitespace-nowrap">{{ $product->publish ? 'Yes' : 'No' }}</td>
+                            <td class="px-6 py-4 border whitespace-nowrap">
+                                <a class="inline-block h-12 px-4 py-3 bg-blue-300 text-white rounded-md"
+                                    href="{{ route('product.show', $product) }}">Show</a>
+                                <a class="inline-block h-12 px-4 py-3 bg-blue-500 text-white rounded-md"
+                                    href="{{ route('product.edit', $product) }}">Edit</a>
+                                <form class="inline m-0 p-0" action="{{ route('product.delete', $product) }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="h-12 px-4 py-3 bg-red-500 text-white rounded-md">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </x-layout>
